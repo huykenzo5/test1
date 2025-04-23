09c20fd23ef52a93890dff827629f7037f80b0c1
huykenzo5 <147372351+huykenzo5@users.noreply.github.com>
2025-04-23 16:30:29 +0700

    Delete online_academy directory

--- online_academy/backend/academy_backend/test/academy_backend_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:academy_backend/academy_backend.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
--- online_academy/frontend/academy_flutter/android/app/src/main/kotlin/com/example/academy_flutter/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.academy_flutter
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
